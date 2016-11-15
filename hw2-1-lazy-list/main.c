53b67e5a13ef79f0b95fbfd56cee14b7d74e6bb3
TobySkarting <toby33022002@gmail.com>
2016-11-15 20:10:11 +0800

    Modify HW2

--- hw2-1-lazy-list/main.c
+++ hw2-1-lazy-list/main.c
@@ -3,15 +3,17 @@
 
 int main()
 {
+	char line[100];
 	int input, created;
 	node_ptr first = create_list();
 
-	while (scanf("%d", &input) == 1)
+	while (fgets(line, sizeof(line), stdin))
 	{
+		if (sscanf(line, "%*[^,],%d", &input) != 1)
+			break;
 		created = make_fib(first, input);
 		print_list_n(first, input);
 		printf(" create %d nodes\n", created);
 	}
-	printf("\nend.\n");
 	return 0;
 }
